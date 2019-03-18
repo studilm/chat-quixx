fdd2b38a178388187e953a690a27142df2ff5ec0
studilm <5081266+studilm@users.noreply.github.com>
2019-03-19 02:43:01 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
-module.exports = (req, res) => {
-  res.end(`Hello from Node.js on Now 2.0!`);
-};
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('chat message', function(msg){
+    console.log('user message:' + msg);
+    io.emit('chat message', msg);
+  });
+  socket.on('wf', function(msg){
+    console.log('wf message:' + msg);
+    io.emit('wf', msg);
+    //socket.broadcast.emit('wf', msg);
+  });
+  socket.on('cross', function(msg){
+    console.log('cross message:' + msg);
+    //io.emit('cross', msg);
+    socket.broadcast.emit('cross', msg);
+  });
+});
+
+http.listen(8080, function(){
+  console.log('listening on *:8080');
+});
